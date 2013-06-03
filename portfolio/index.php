d0639b9c1fe319d5dffb6b12bf94aa94fae03f1e
hshirani <hshirani@stkfactory.(none)>
2013-06-03 13:52:14 +0000

    - Installed google analytics module - added directory for digital art - ported portfolio content into drupal node - Installed a bunch of dependency modules for the main shopping cart

--- portfolio/index.php
+++ portfolio/index.php
@@ -6,40 +6,39 @@
 <h1>Hassan Shirani</h1>
 <h3>Resume</h3>
 <ul>
-	<li><a href="hassan-shirani-resume-current.pdf">PDF</a></li>
+	<li><a href="hassan-shirani-resume-current.pdf">PDF Resume</a></li>
+	<li><a href="http//www.odesk.com/users/~014f27c0dbe8c2519b">oDesk Profile</a></li>
 	<!-- <li><a href="Hassan_Shirani_Resume.doc">MS WORD</a></li> -->
 </ul>
 <h3>Portfolio</h3>
 <ul>
 	<li>
-		Code Samples
+		Code In Production
 		<ul>
-			<li><a href="http://www.vedaproject.org" target="new">The Veda Project, Inc</a> is a restful api driven learning mangement system. Lessons are delivered in the form of text/html embedded with "interactive learning objects." Below are two resource classes and their api interfaces.
-				<ul>
-					<a href="source/Lesson.phps"><li>Lesson Class</a></li>
-					<a href="source/lesson.phps"><li>Lesson Interface</a></li>
-					<a href="source/Ilo.phps"><li>ILO (Interactive Learning Object) Class</a></li>
-					<a href="source/ilosByLesson.phps"><li>ILO Interface</a></li>
-				</ul>
-			</li>
-			<li><a href="source/Transaction.phps">A basic PHP class that interfaces with a payment gateway.</a></li>
-			<li><a href="source/change_lesson_order.sqls">A PLPGSQL stored procedure that updates the position of elements in a list represented by a relational database table.</a></li>
-			<li><a href="source/p1cupload">A Bash script released under the BSD liscense that interfaces with an EMC/ATMOS cloud storage stack.</a></li>
-			<li><a href="source/deb_install/">A Debian install script which facilitates the installation of the above script.</a></li>
-			<li><a href="source/cloud_web/">A proof of concept implementing streaming from an EMC / ATMOS cloud storage stack directly to a web browser using a webserver (apache) as a proxy.</a></li>
-			<li><a href="source/dev.api.conf">A custom Apache virtual host configuration file.</a></li>
-			<li><a href="source/A3.java.php">Sample Java code submitted as coursework.</a></li>
+			<li><a href="http://friedrichlink.friedrich.com" target="new">A home automation component for window / room air conditioners.</a></li>
+			<li><a href="http://www.techsurance.com" target="new">A site ported from html/php to a Drupal CMS template (PHPTemplate) (Copyright Techsurance, LLC).</a></li>
+			<li><a href="http://www.howellandsabrin.com" target="new">A basic website created according to customer's design specifications.</a></li>
+			<li><a href="http://ansi.zenfactory.org" target="new">A basic implementation of a lightbox.</a></li>
 		</ul>
 	</li>
 	<li>
-		Code In Production
+		Code Samples
 		<ul>
-			<li><a href="http://www.techsurance.com" target="new">A site ported from html/php to a Drupal CMS template (PHPTemplate) (Copyright Techsurance, LLC).</a></li>
-			<li><a href="http://www.howellandsabrin.com" target="new">A basic website created according to customer's design specifications.</a></li>
-			<li><a href="http://www.zenfactory.org/artscene" target="new">A basic implementation of a lightbox.</a></li>
-			<li><a href="http://friedrichlink.friedrich.com" target="new">A home automation component for window / room air conditioners.</a></li>
-			<!-- <li><a href="http://www.zenfactory.org/cloud/gallery/index.php" target="new">A proof of concept rendering / streaming images directly from an EMC / ATMOS cloud storage stack.</a></li> -->
-			<!-- <li><a href="http://www.zenfactory.org/cloud/gallery/video.php" target="new">A proof of concept rendering / streaming videos directly from an EMC / ATMOS cloud storage stack.</a></li> -->
+			<li><a href="http://mvp.vedaproject.org" target="new">The Veda Project, Inc</a> is a restful api driven learning mangement system. Lessons are delivered in the form of text/html embedded with "interactive learning objects." Below are two resource classes and their api interfaces.
+				<ul>
+					<li><a href="http://www.zenfactory.org/portfolio/source/Lesson.phps">Lesson Class</a></li>
+					<li><a href="http://www.zenfactory.org/portfolio/source/lesson.phps">Lesson Interface</a></li>
+					<li><a href="http://www.zenfactory.org/portfolio/source/Ilo.phps">ILO (Interactive Learning Object) Class</a></li>
+					<li><a href="http://www.zenfactory.org/portfolio/source/ilosByLesson.phps">ILO Interface</a></li>
+				</ul>
+			</li>
+			<li><a href="http://www.zenfactory.org/portfolio/source/Transaction.phps">A basic PHP class that interfaces with a payment gateway.</a></li>
+			<li><a href="http://www.zenfactory.org/portfolio/source/change_lesson_order.sqls">A PLPGSQL stored procedure that updates the position of elements in a list represented by a relational database table.</a></li>
+			<li><a href="http://www.zenfactory.org/portfolio/source/p1cupload">A Bash script released under the BSD liscense that interfaces with an EMC/ATMOS cloud storage stack.</a></li>
+			<li><a href="http://www.zenfactory.org/portfolio/source/deb_install/">A Debian install script which facilitates the installation of the above script.</a></li>
+			<li><a href="http://www.zenfactory.org/portfolio/source/cloud_web/">A proof of concept implementing streaming from an EMC / ATMOS cloud storage stack directly to a web browser using a webserver (apache) as a proxy.</a></li>
+			<li><a href="http://www.zenfactory.org/portfolio/source/dev.api.conf">A custom Apache virtual host configuration file.</a></li>
+			<li><a href="http://www.zenfactory.org/portfolio/source/A3.java.php">Sample Java code submitted as coursework.</a></li>
 		</ul>
 	</li>
 </ul>
